2f3fb08456a728bf5716f89337c75a126ef32d23
i52gohel <55751879+i52gohel@users.noreply.github.com>
2019-11-26 17:36:09 +0100

    Add files via upload

---
+++ codificacion/cita.hpp
@@ -0,0 +1,39 @@
+#ifndef _CITA_HPP
+#define _CITA_HPP
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Cita {
+	private:
+		string _dni, _nombre, _apellidos, _fechaCita;
+
+	public:
+
+		Cita(){};
+
+		~Cita(){};
+
+		//Métodos get
+		inline string getDni() const { return _dni; }
+		inline string getNombre() const { return _nombre; }
+		inline string getApellidos() const { return _apellidos; }
+		inline string getFechaCita() const { return _fechaCita; }
+
+		//Métodos set
+
+		inline void setDni(const std::string &cadena) { _dni = cadena};
+		inline void setNombre(const std::string &cadena) { _nombre = cadena; }
+		inline void setApellidos(const std::string &cadena) { _apellidos = cadena; }
+		inline void setFechaCita(const std::string &cadena) { _fechaCita = cadena};
+
+
+		//Otros métodos
+		void ActualizarVectorCitas(const std::vector <std::string> &v);
+		void printVectorCitas(const std::vector <std::string> &v);
+
+};
+
+#endif
